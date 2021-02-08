f8371294aeaa68ce1e5f7e212517b876aec00f81
CristoferGarcia1984 <60959942+CristoferGarcia1984@users.noreply.github.com>
2021-02-08 12:52:47 +0100

    Initial react-app set up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
+import { Switch, Route } from "react-router-dom"; 
 import './App.css';
+import React from "react"; 
+import ROUTES from "./utils/routes"; 
+import ReactDOM from "react-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Switch>
+      <Route path={ROUTES.MAIN}>
+        <div>THIS IS THE MAIN PAGE</div>
+      </Route>
+    </Switch>
+  )
 }
 
 export default App;
