fd13f382534ebb397837e6a701f1da9b55accb84
CristoferGarcia1984 <60959942+CristoferGarcia1984@users.noreply.github.com>
2021-02-09 12:57:38 +0100

    Change styles for header and link to css files

--- src/components/common/HeaderComponent/HeaderComponent.js
+++ src/components/common/HeaderComponent/HeaderComponent.js
@@ -1,15 +1,12 @@
 import React from "react";
-import { Button } from "react-bootstrap";
-import Image from "react-bootstrap/Image";
 import SignInSignUp from "../SignIn-signUp/SignIn-signUp";
 import Menu from "../Menu/Menu";
+import "./HeaderComponent.scss"; 
 
 export default function HeaderComponent() {
   return (
     <div className="navHeader">
       <Menu />
-      {/*<Image className="searchIcon" src="/assets/icons/search.svg"></Image>*/}
-
       <SignInSignUp />
     </div>
   );
