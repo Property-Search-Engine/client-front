b6a1f3104ff56f0fda3147e2a4fdd098d91463a4
Damantino <perezgarciadaniel1@gmail.com>
2021-02-09 20:25:00 +0100

    feat/Home/Office Toggle

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
-import { Switch, Route } from "react-router-dom"; 
-import './App.css';
-import React from "react"; 
-import ROUTES from "./utils/routes"; 
+import { Switch, Route } from "react-router-dom";
+import "./App.css";
+import React from "react";
+import ROUTES from "./utils/routes";
 import ReactDOM from "react-dom";
+import HomeOfficeToggle from "./components/common/HomeOfficeToggle/HomeOfficeToggle";
 
 function App() {
+  const [toggled, setToggled] = React.useState(false);
+  const handleClick = () => {
+    setToggled((s) => !s);
+  };
   return (
     <Switch>
       <Route path={ROUTES.MAIN}>
-        <div>THIS IS THE MAIN PAGE</div>
+        <div className="app">
+          <HomeOfficeToggle toggled={toggled} onClick={handleClick} />
+        </div>
       </Route>
     </Switch>
-  )
+  );
 }
 
 export default App;
