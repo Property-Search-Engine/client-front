943ae4dfe7df235a207ac707256a30e6f8538dec
Álex <a.lerida@yahoo.com>
2021-02-10 16:52:36 +0100

    Bookings

--- src/components/pages/Main/Main.js
+++ src/components/pages/Main/Main.js
@@ -1,17 +1,30 @@
-import React, { useState } from 'react'; 
-import Properties from '../../common/Properties/Properties'; 
-import PropertyDetails from '../../common/PropertyDetails/PropertyDetails';
-import  { propertyEx, propertyEx2, propertyEx3 } from '../../../utils/mockOfProperties'; 
+import React, { useState } from "react";
+import Properties from "../../common/Properties/Properties";
+import PropertyDetails from "../../common/PropertyDetails/PropertyDetails";
+import Bookings from "../../common/Bookings/Bookings";
+import {
+  propertyEx,
+  propertyEx2,
+  propertyEx3,
+} from "../../../utils/mockOfProperties";
 
 export default function Main() {
+  const [main, setMain] = useState(true);
+  const [singlePropertyId, setsinglePropertyId] = useState("");
+  const properties = [propertyEx, propertyEx2, propertyEx3];
 
-    const [main, setMain] = useState(true);
-    const [singlePropertyId, setsinglePropertyId] = useState("");
-    const properties = [propertyEx, propertyEx2, propertyEx3]; 
-
-    return (
-        <div className="mainContainer">
-            {main ? <Properties properties={properties} setMain={setMain} setsinglePropertyId={setsinglePropertyId}/> : <PropertyDetails setMain={setMain} id={singlePropertyId}/>}
-        </div>
-    )
+  return (
+    <div className="mainContainer">
+      <Bookings />
+      {main ? (
+        <Properties
+          properties={properties}
+          setMain={setMain}
+          setsinglePropertyId={setsinglePropertyId}
+        />
+      ) : (
+        <PropertyDetails setMain={setMain} id={singlePropertyId} />
+      )}
+    </div>
+  );
 }
