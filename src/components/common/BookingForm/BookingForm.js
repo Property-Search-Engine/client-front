791695f93f399aa2a236068d0c33c87169b5a531
CristoferGarcia1984 <60959942+CristoferGarcia1984@users.noreply.github.com>
2021-02-14 12:37:59 +0100

    Change class component to rfc and prepare component for submit

--- src/components/common/BookingForm/BookingForm.js
+++ src/components/common/BookingForm/BookingForm.js
@@ -1,37 +1,54 @@
-import React from "react";
+import React, {useState} from "react";
 
 import "./BookingForm.sass";
 
-export default class BookingForm extends React.Component {
-  constructor(props) {
-    super(props);
+export default function BookingForm (props) {
+
+  const [name, setname] = useState("")
+  const [message, setmessage] = useState("")
+  const [phone, setphone] = useState("")
+
+  function handleSubmit (e) {
+    e.preventDefault(); 
+
+    if (name !== "" && message !== "" && phone !== "" ) {
+      //send the booking form()
+    }
+
   }
 
-  render() {
     return (
       <div className="BookingForm">
         <p>Get in touch with the advertiser</p>
 
-        <form>
-          <div className="field">
-            <div className="title">
-              <i class="fas fa-comment-alt"></i>
-              <p>Message</p>
-            </div>
-            <textarea placeholder="Write your message here..."></textarea>
-          </div>
-
+        <form onSubmit={handleSubmit}>
+  
           <div className="field">
             <div className="title">
               <i class="fas fa-user"></i>
               <p>Name</p>
             </div>
-            <input placeholder="John Doe"></input>
+            <input value={name} placeholder="John Doe" onChange={(e) => setname(e.target.value)}></input>
+          </div>
+
+          <div className="field">
+            <div className="title">
+              <i class="fas fa-user"></i>
+              <p>Phone Number</p>
+            </div>
+            <input value={phone} placeholder="645307574" onChange={(e) => setphone(e.target.value)}></input>
+          </div>
+
+          <div className="field">
+            <div className="title">
+              <i class="fas fa-comment-alt"></i>
+              <p>Message</p>
+            </div>
+            <textarea value={message} placeholder="Write your message here..." onChange={(e) => setmessage(e.target.value)}></textarea>
           </div>
 
           <button>Send Message</button>
         </form>
       </div>
     );
-  }
 }
