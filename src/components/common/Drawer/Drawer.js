92625037e6cf6f5707ee54ae9b2143a0138b9d32
Sukaina <sukaina.ab220@gmail.com>
2021-02-14 18:25:05 +0100

    Push of Drawer

--- src/components/common/Drawer/Drawer.js
+++ src/components/common/Drawer/Drawer.js
@@ -1,154 +1,51 @@
 import React from "react";
-import SignInSignUp from "../SignIn-signUp/SignIn-signUp";
 import "../Drawer/Drawer.scss";
-import clsx from "clsx";
-import { makeStyles, useTheme } from "@material-ui/core/styles";
-import Drawer from "@material-ui/core/Drawer";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import AppBar from "@material-ui/core/AppBar";
-import { List, Toolbar, Typography, Divider, ListItem, ListItemText } from "@material-ui/core";
-import IconButton from "@material-ui/core/IconButton";
-import MenuIcon from "@material-ui/icons/Menu";
-import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
-import ChevronRightIcon from "@material-ui/icons/ChevronRight";
 
-const drawerWidth = 230;
+import 'antd/dist/antd.css';
+import { Drawer, Button } from 'antd';
 
-const useStyles = makeStyles((theme) => ({
-    root: {
-        display: "flex",
-        color: "black",
-    },
-    appBar: {
-        transition: theme.transitions.create(["margin", "width"], {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.leavingScreen,
-        }),
-    },
-    appBarShift: {
-        width: `calc(100% - ${drawerWidth}px)`,
-        marginLeft: drawerWidth,
-        transition: theme.transitions.create(["margin", "width"], {
-            easing: theme.transitions.easing.easeOut,
-            duration: theme.transitions.duration.enteringScreen,
-        }),
-    },
-    menuButton: {
-        marginRight: theme.spacing(2),
-        color: theme.black
-    },
-    hide: {
-        display: "none",
-    },
-    drawer: {
-        width: drawerWidth,
-        flexShrink: 0,
-    },
-    drawerPaper: {
-        width: drawerWidth,
-    },
-    drawerHeader: {
-        display: "flex",
-        alignItems: "center",
-        padding: theme.spacing(0, 1),
-        // necessary for content to be below app bar
-        ...theme.mixins.toolbar,
-        justifyContent: "flex-end",
-    },
-    content: {
-        flexGrow: 1,
-        padding: theme.spacing(3),
-        transition: theme.transitions.create("margin", {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.leavingScreen,
-        }),
-        marginLeft: -drawerWidth,
-    },
-    contentShift: {
-        transition: theme.transitions.create("margin", {
-            easing: theme.transitions.easing.easeOut,
-            duration: theme.transitions.duration.enteringScreen,
-        }),
-        marginLeft: 0,
-    },
-}));
 
-export default function PersistentDrawerLeft() {
-    const classes = useStyles();
-    const theme = useTheme();
-    const [open, setOpen] = React.useState(false);
+export default class DrawerMenu extends React.Component {
+    state = { visible: false };
 
-    const handleDrawerOpen = () => {
-        setOpen(true);
+    showDrawer = () => {
+        this.setState({
+            visible: true,
+        });
     };
 
-    const handleDrawerClose = () => {
-        setOpen(false);
+    onClose = () => {
+        this.setState({
+            visible: false,
+        });
     };
 
-    return (
-        <div className={classes.root}>
-            <CssBaseline />
-            <AppBar
-                position="fixed"
-                className={clsx(classes.appBar, {
-                    [classes.appBarShift]: open,
-                })}
-            >
-                <Toolbar>
-                    <IconButton
-                        color="inherit"
-                        aria-label="open drawer"
-                        onClick={handleDrawerOpen}
-                        edge="start"
-                        className={clsx(classes.menuButton, open && classes.hide)}
+    render() {
+        return (
+            <div className="site-drawer-render-in-current-wrapper">
+                <button className="openDrawer" style={{ zIndex: '1' }}
+                    type="primary" onClick={this.showDrawer}>
+                    <img className="searchIcon" src="/assets/icons/menu.svg" />
+                </button>
 
-                    >
-                        <MenuIcon />
-                    </IconButton>
-                    <Typography noWrap>
-                        {/* SignIn-SingUp */}
 
-                        <SignInSignUp />
+                <Drawer 
+                    
+                    width="200px"
+                    distance
+                    placement="left"
+                    closable={false}
+                    onClose={this.onClose}
+                    visible={this.state.visible}
+                    getContainer={false}
+                    style={{ position: '' }}
+                >
+                    <div></div>
+                </Drawer>
 
-                    </Typography>
-                </Toolbar>
-            </AppBar>
-            <Drawer
-                className={classes.drawer}
-                variant="persistent"
-                anchor="left"
-                open={open}
-                classes={{
-                    paper: classes.drawerPaper,
-                }}
-            >
-                <div className={classes.drawerHeader}>
-                    <IconButton onClick={handleDrawerClose}>
-                        {theme.direction === "ltr" ? (
-                            <ChevronLeftIcon />
-                        ) : (
-                                <ChevronRightIcon />
-                            )}
-                    </IconButton>
-                </div>
-                <Divider />
-                <List>
-                    {["Favorites", "Bookings", "Log out"].map((text) => (
-                        <ListItem button key={text}>
-                            <ListItemText primary={text} />
-                        </ListItem>
-                    ))}
-                </List>
-            </Drawer>
-            <main
-                className={clsx(classes.content, {
-                    [classes.contentShift]: open,
-                })}
-            >
-                <div className={classes.drawerHeader} />
 
-            </main>
-        </div>
-    );
+
+            </div>
+        );
+    }
 }
--- src/components/common/HeaderComponent/HeaderComponent.js
+++ src/components/common/HeaderComponent/HeaderComponent.js
@@ -1,15 +1,19 @@
 import React from "react";
 import "../HeaderComponent/HeaderComponent.scss";
 //import Menu from "../Menu/Menu";
-import MiniDrawer from "../Drawer/Drawer"
+import DrawerMenu from "../Drawer/Drawer";
 export default function HeaderComponent() {
   return (
+
     <div className="navHeader">
       {/*
       <Image className="searchIcon" src="/assets/icons/search.svg"></Image>
-      <Menu />
+     <Menu />
       */}
-      <MiniDrawer />
+      <DrawerMenu />
+
+ 
+      
     </div>
   );
 }
